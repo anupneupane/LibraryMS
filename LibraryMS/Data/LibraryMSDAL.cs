a93a3eb6a50fa22ffa88b8703faf030ea28fff13
Anup Neupane <anupbro@gmail.com>
2022-10-17 22:06:46 -0400

    add book model and data class files

---
+++ LibraryMS/Data/LibraryMSDAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LibraryMS.Models;
+
+namespace LibraryMS.Data
+{
+    public class LibraryMSDAL : IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void saveBooks(Book Book)
+        {
+            using (LibraryMSDBContext ctx = new LibraryMSDBContext())
+            {
+                ctx.Books.Add(Book);
+                ctx.SaveChanges();
+            }
+        }
+
+        public List<Book> getBooks()
+        {
+            using (LibraryMSDBContext ctx = new LibraryMSDBContext())
+            {
+                return ctx.Books.ToList();
+            }
+        }
+
+        public Book getItemByID(int id)
+        {
+            using (LibraryMSDBContext ctx = new LibraryMSDBContext())
+            {
+                return ctx.Books.Where(book => book.Id == id).First();
+
+            }
+
+        }
+
+        public LibraryMSDAL()
+        {
+        }
+    }
+}
+
